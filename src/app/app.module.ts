66125cac01b47e8eabe34a8e57c69d658d8dbaf9
AlbertoCamachoMuñoz <c4max0@gmail.com>
2022-01-14 14:48:43 +0100

    Fin seccion 3 -iniciando proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
+
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+
+import { PagesComponent } from './pages/pages.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
+
+    PagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+// clase 20 Agular Principales Avanzado
+// clase 21 rutas hijas Agular Avanzado
