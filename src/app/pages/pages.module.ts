e2ccfdb6d50eb22b3df32ed59fa5e0a268b261ba
AlbertoCamachoMunoz <33063094+AlbertoCamachoMunoz@users.noreply.github.com>
2022-01-15 23:14:19 +0100

    Update pages.module.ts
    // componentes desde github

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+// componentes desde github
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
