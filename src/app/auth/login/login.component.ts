ea1d29c9ace4c04a50fca2948843dcc287a4eee6
AlbertoCamachoMuñoz <c4max0@gmail.com>
2022-02-01 17:18:26 +0100

    Iniciando login en frontend

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,19 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import Swal from 'sweetalert2'
+
+import { UsuarioService } from '../../services/usuario.service'
 
 @Component({
 	selector: 'app-login',
 	templateUrl: './login.component.html',
 	styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
-	constructor(private router:Router) {}
+export class LoginComponent {
+	public formSubmitted = false
 
-	ngOnInit(): void {}
+	public loginForm = this.fb.group({
+		email: ['test@gmail.com', [Validators.required, Validators.email]],
+		password: ['123456', [Validators.required]],
+		remember:[false]
+	});
 
-  login() {
-    console.log('autenticado');
-    this.router.navigateByUrl('/');
-    
-  }
+	constructor(
+		private router: Router,
+		private fb: FormBuilder,
+		private usuarioService: UsuarioService
+	) {}
+
+	login() {
+		console.log(this.loginForm.valid)
+		// this.router.navigateByUrl('/')
+		if (!this.loginForm.valid) return
+		else
+			this.usuarioService.loginUsuario(this.loginForm.value).subscribe({
+				next(resp) {
+					console.log(resp)
+				},
+				error(err) {
+					Swal.fire({
+						title: 'Error!',
+						text: `${err.error.msg} USUARIOS`,
+						icon: 'error',
+						confirmButtonText: 'ok'
+					})
+				}
+			})
+	}
 }
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { RegisterForm } from '../interfaces/register-form.interface';
 import { environment } from '../../environments/environment';
+import { RegisterForm } from '../interfaces/register-form.interface';
+import { LoginForm } from '../interfaces/login-form.interface';
 
 const base_url = environment.base_url; 
 
@@ -13,8 +14,15 @@ export class UsuarioService {
 
 	crearUsuario(formData: RegisterForm) {
 		console.log('creando Usuario')
-        console.log(formData)
+		console.log(formData)
 
-        return this.http.post(`${base_url}/usuarios`, formData)
+		return this.http.post(`${base_url}/usuarios`, formData)
+	}
+
+	loginUsuario(formData: LoginForm) {
+		console.log('login Usuario')
+		console.log(formData)
+
+		return this.http.post(`${base_url}/login`, formData)
 	}
 }
